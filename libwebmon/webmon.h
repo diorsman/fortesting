591026469e250af00084073823a5361979d9914f
dugang@188.com <dugang@188.com@89d0a82a-9051-0410-9afa-0d9e86537d73>
2008-07-25 10:07:01 +0000

    add shock stat.

--- libwebmon/webmon.h
+++ libwebmon/webmon.h
@@ -26,6 +26,7 @@
 
 typedef const char **(*TEXTINFO_CB)(void *arg);
 typedef void (*FREE_TEXTINFO_CB)(void *arg);
+
 typedef int (*SAMPLE)(void *arg, double *ret);
 struct webmon_graph_t {
     /* for entire graph */
@@ -39,12 +40,28 @@ struct webmon_graph_t {
     void *args[LINE_COUNT_MAX];
 };
 
+struct webmon_graph_data_t {
+    /* for entire graph */
+    const char *desc;
+    int nsample;
+    int line_count;
+    double high_limit;
+    double low_limit;
+    /* for each lines */
+    const char *line_labels[LINE_COUNT_MAX];
+    double *values[LINE_COUNT_MAX];
+};
+typedef const struct webmon_graph_data_t ** (*GRAPH_CB)(void *arg);
+typedef void (*FREE_GRAPH_CB)(void *arg);
+
 int webmon_init(void);
 void *webmon_create(const char *title, const char *ip, int port, int nsample, 
                     int interval, int cpu, int mem, int netif);
 void webmon_set_textinfo_callback(void *webmon, TEXTINFO_CB cb, void *arg);
 void webmon_set_free_textinfo_callback(void *webmon, FREE_TEXTINFO_CB cb);
 int webmon_addgraph(void *webmon, const struct webmon_graph_t *graph);
+void webmon_set_graph_callback(void *webmon, GRAPH_CB cb, void *arg);
+void webmon_set_free_graph_callback(void *webmon, FREE_GRAPH_CB cb);
 int webmon_run(void *webmon);
 
 #endif  /* _WEBMON_H_ */
